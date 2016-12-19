84ea7a1a71e7657790fe0d66770b14e58100aa92
letrungkien0210 <letrungkien0210@gmail.com>
2016-12-19 10:10:38 +0700

    Push the project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # own_twitter_app
-Build a Twitter Like app using the MVC design pattern"
+Build a Twitter Like app using the MVC design pattern
---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// Database URL 
+module.exports = {
+    // Connect with MongoDB on local machine 
+    'url': 'mongodb://localhost:27017/mvc-app'
+}; 
